2039379f0331d3db7b2e4e1244bff370744e7c82
Eric Haberstroh <gpg@erixpage.de>
2023-10-31 11:19:58 +0100

    Add Stimulus frontend

---
+++ src/Controller/FrontpageController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FrontpageController extends AbstractController
+{
+    #[Route('/', name: 'app_frontpage')]
+    public function index(): Response
+    {
+        return $this->render('frontpage/index.html.twig', [
+            'controller_name' => 'FrontpageController',
+        ]);
+    }
+}
